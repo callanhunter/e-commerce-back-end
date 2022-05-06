349ba941dd719eae0482e560526d6a4e9cf84f56
callanhunter <callan57hunter@gmail.com>
2022-05-05 18:13:49 -0700

    add more content to product-routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,23 +6,37 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  ProductTag.findAll({
+    include: [Product]
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findByPk(req.params.id ,{
+    include: [Product]
+  }) .then(data => res.json(data))
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create(req.body)
+  .then(data => res.json(data))
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update({
+    id: req.params.id,
+  }, req.body) .then(data => res.json(data))
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    id: req.params.id,
+  }) .then(data => res.json(data))
 });
 
 module.exports = router;
